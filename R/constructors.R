9b6c6a0a089d12edf0ea4c35ac1e6fd495e99213
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-02-06 03:48:02 +0900

    :sparkles: Add graph_from_symbolic_edgelist()

--- R/constructors.R
+++ R/constructors.R
@@ -1,4 +1,21 @@
+#' Create new graph
+#'
+#' @param el two-column matrix
+#' @param directed logical
+#' @rdname constructors
+#' @export
 graph_from_edgelist = function(el, directed = TRUE) {
   edges = as.numeric(t(el)) - 1
   igraph_create(edges, directed = directed)
+}
+
+#' @rdname constructors
+#' @export
+graph_from_symbolic_edgelist = function(el, directed = TRUE) {
+  edges = as.vector(t(el))
+  labels = unique(edges)
+  ids = seq_along(labels) - 1
+  g = igraph_create(ids[edges], directed = directed)
+  g$V$name = labels
+  g
 }
