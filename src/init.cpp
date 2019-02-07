668eb3cc43a511d52f7da9d76e668756254f6250
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-02-08 01:22:00 +0900

    :fire: Do not use igraph_cattributes

--- src/init.cpp
+++ src/init.cpp
@@ -1,7 +1,7 @@
 // [[Rcpp::plugins(cpp14)]]
 #include <Rcpp.h>
 
-#include <igraph/igraph_attributes.h>
+#include <igraph/igraph_error.h>
 
 void error_handler(const char* reason, const char* file, int line, int igraph_errno) {
   igraph_error_handler_printignore(reason, file, line, igraph_errno);
@@ -11,5 +11,4 @@ void error_handler(const char* reason, const char* file, int line, int igraph_er
 // [[Rcpp::init]]
 void igraphlite_init(DllInfo *dll) {
   igraph_set_error_handler(&error_handler);
-  igraph_i_set_attribute_table(&igraph_cattribute_table);
 }
