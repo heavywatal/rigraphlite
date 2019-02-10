66c9d2a953285cdfb739dbde7719a441e18f0974
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-02-10 22:57:59 +0900

    :sparkles: Add show() method

--- R/igraphlite-package.R
+++ R/igraphlite-package.R
@@ -8,3 +8,13 @@
 }
 
 Rcpp::loadModule("igraph", TRUE)
+
+# It is actually an RC, but the super class "C++Object" has S4 show()
+setClass("Rcpp_IGraph")
+setMethod("show", "Rcpp_IGraph", function(object){
+  methods::callNextMethod(object)
+  cat("$V  ")
+  utils::str(object$V)
+  print(object$as_data_frame())
+  invisible(object)
+})
