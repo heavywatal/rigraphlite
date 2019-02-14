87f4ddc26b04551e7f2fe6b679ac8d155ba08616
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-02-14 23:57:52 +0900

    :sparkles: Add as_igraph()

--- R/igraphlite-package.R
+++ R/igraphlite-package.R
@@ -22,8 +22,35 @@ setMethod("show", "Rcpp_IGraph", function(object){
 #' IGraph class
 #'
 #' @param x object to be tested.
+#'
 #' @rdname igraph-class
 #' @export
 is_igraph = function(x) {
   inherits(x, "Rcpp_IGraph")
+}
+
+#' @rdname igraph-class
+#' @export
+as_igraph = function(x) UseMethod("as_igraph")
+
+#' @rdname igraph-class
+#' @export
+as_igraph.default = function(x) {
+  graph_create(x)
+}
+
+#' @rdname igraph-class
+#' @export
+as_igraph.matrix = function(x) {
+  if (is.double(x)) {
+    graph_from_edgelist(x)
+  } else {
+    graph_from_symbolic_edgelist(x)
+  }
+}
+
+#' @rdname igraph-class
+#' @export
+as_igraph.data.frame = function(x) {
+  graph_from_data_frame(x)
 }
