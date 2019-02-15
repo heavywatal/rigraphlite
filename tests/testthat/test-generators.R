1eeec5ae3eabc962aaf9c9ab715a1496058000a6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-02-15 23:15:37 +0900

    :white_check_mark: Add tests for igraph-class

--- tests/testthat/test-generators.R
+++ tests/testthat/test-generators.R
@@ -12,3 +12,14 @@ test_that("generator functions work", {
   expect_s4_class(graph_from_data_frame(el), "Rcpp_IGraph")
   expect_error(graph_create(seq_len(n - 1L)))
 })
+
+test_that("igraph-class works", {
+  n = 8L
+  edges = seq_len(n)
+  el = matrix(edges, ncol = 2L)
+  df = as.data.frame(el)
+  expect_true(is_igraph(as_igraph(edges)))
+  expect_true(is_igraph(as_igraph(el)))
+  expect_true(is_igraph(as_igraph(df)))
+  expect_output(show(as_igraph(df)))
+})
