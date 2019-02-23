2e0810b1e6ec98ba5ae4262cd048c49c0dd1e679
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-02-23 23:51:08 +0900

    :memo: Add document for IGraph class

--- R/igraphlite-package.R
+++ R/igraphlite-package.R
@@ -9,21 +9,35 @@
 
 Rcpp::loadModule("igraph", TRUE)
 
-# It is actually an RC, but the super class "C++Object" has S4 show()
-setClass("Rcpp_IGraph")
-setMethod("show", "Rcpp_IGraph", function(object){
-  methods::callNextMethod(object)
-  cat("$V  ")
-  utils::str(object$Vattr)
-  print(object$as_data_frame)
-  invisible(object)
-})
+#' IGraph class: thin wrapper of `igraph_t`.
+#'
+#' @field V Vertex IDs in a numeric vector
+#' @field E Edge IDs in a numeric vector
+#' @field Vattr Vertex attributes in a data.frame
+#' @field Eattr Edge attributes in a data.frame
+#'
+#' @field from The first column of the edge list.
+#' @field to The second column of the edge list.
+#' @field oi The index of the edge list by the first column.
+#' @field ii The index of the edge list by the second column.
+#' @field os The pointers to the outgoing edges.
+#' @field is The pointers to the incoming edges.
+#'
+#' @seealso [graph_from_edgelist()], [graph_from_data_frame], [graph_tree()]
+#'   for object instanciation.
+#' @source <https://github.com/igraph/igraph/blob/master/include/igraph_datatype.h>
+#' @docType class
+#' @keywords data
+#' @format Reference Class (RC) exposed by Rcpp Modules.
+#' @name IGraph
+#' @rdname igraph-class
+NULL
 
-#' IGraph class
+#' Check if an object is IGraph class
 #'
 #' @param x object to be tested.
 #'
-#' @rdname igraph-class
+#' @rdname is_igraph
 #' @export
 is_igraph = function(x) {
   inherits(x, "Rcpp_IGraph")
---
+++ R/s4-method.R
@@ -0,0 +1,9 @@
+# It is actually an RC, but the super class "C++Object" has S4 show()
+setClass("Rcpp_IGraph")
+setMethod("show", "Rcpp_IGraph", function(object){
+  methods::callNextMethod(object)
+  cat("$V  ")
+  utils::str(object$Vattr)
+  print(object$as_data_frame)
+  invisible(object)
+})
