68bf8dff68c87b1cc564351fceca42e41f63eeb9
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-11 16:09:21 +0900

    :art: Separate basic and generators from igraph.cpp

--- src/generators.cpp
+++ src/generators.cpp
@@ -1,4 +1,8 @@
 #include "igraph.hpp"
+#include "vector.hpp"
+
+#include <igraph/igraph_interface.h>
+#include <igraph/igraph_constructors.h>
 
 namespace impl {
 
@@ -69,6 +73,56 @@ namespace impl {
     return g;
   }
 
+}
+
+IGraph::IGraph(): data_(std::make_unique<igraph_t>()) {}
+
+IGraph::~IGraph() noexcept {
+  if (data_) igraph_destroy(data_.get());
+}
+
+IGraph::IGraph(const IGraph& other) noexcept: IGraph::IGraph() {
+  igraph_copy(data_.get(), other.data_.get());
+  Vattr_ = other.Vattr_;
+  Eattr_ = other.Eattr_;
+}
+
+IGraph::IGraph(IGraph&& other) = default;
+
+IGraph::IGraph(int n, bool directed): IGraph::IGraph() {
+  igraph_empty(data_.get(), n, directed);
+  init_attr();
+}
+
+IGraph::IGraph(const Rcpp::NumericVector& edges, int n, bool directed): IGraph::IGraph() {
+  igraph_create(data_.get(), ISelectorInPlace(edges).data(), n, directed);
+  init_attr();
+}
+
+IGraph::IGraph(int n, int mode, double center): IGraph::IGraph() {
+  // double center to distinguish overloaded contructors (vs tree).
+  igraph_star(data_.get(), n, static_cast<igraph_star_mode_t>(mode), static_cast<int>(center));
+  init_attr();
+}
+
+IGraph::IGraph(const Rcpp::NumericVector& dim, int nei, bool directed, bool mutual, bool circular): IGraph::IGraph() {
+  igraph_lattice(data_.get(), ISelectorInPlace(dim).data(), nei, directed, mutual, circular);
+  init_attr();
+}
+
+IGraph::IGraph(int n, bool directed, bool mutual, bool circular): IGraph::IGraph() {
+  igraph_ring(data_.get(), n, directed, mutual, circular);
+  init_attr();
+}
+
+IGraph::IGraph(int n, int children, int mode): IGraph::IGraph() {
+  igraph_tree(data_.get(), n, children, static_cast<igraph_tree_mode_t>(mode));
+  init_attr();
+}
+
+IGraph::IGraph(int n, bool directed, bool loops): IGraph::IGraph() {
+  igraph_full(data_.get(), n, directed, loops);
+  init_attr();
 }
 
 //' @param df data.frame that includes an edgelist and edge attributes.
