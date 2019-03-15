5ad0542264e5d138823eb9a239a34980bf020d8f
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-16 00:22:20 +0900

    :sparkles: Add vertex_attr() and edge_attr() for compatibility

--- R/attributes.R
+++ R/attributes.R
@@ -1,3 +1,51 @@
+#' Get and set vertex and edge attributes
+#'
+#' @inheritParams common_params
+#' @param name Name of the attribute to query or set.
+#'   If missing, all the attributes are returned or replaced.
+#' @param value A vector or data.frame.
+#' @rdname attributes
+#' @export
+vertex_attr = function(graph, name) {
+  if (missing(name)) {
+    graph$Vattr
+  } else {
+    graph$Vattr[[name]]
+  }
+}
+
+#' @rdname attributes
+#' @export
+`vertex_attr<-` = function(graph, name, value) {
+  if (missing(name)) {
+    graph$Vattr = value
+  } else {
+    graph$Vattr[[name]] = value
+  }
+  graph
+}
+
+#' @rdname attributes
+#' @export
+edge_attr = function(graph, name) {
+  if (missing(name)) {
+    graph$Eattr
+  } else {
+    graph$Eattr[[name]]
+  }
+}
+
+#' @rdname attributes
+#' @export
+`edge_attr<-` = function(graph, name, value) {
+  if (missing(name)) {
+    graph$Eattr = value
+  } else {
+    graph$Eattr[[name]] = value
+  }
+  graph
+}
+
 #' Convesion between vertex IDs and names
 #'
 #' @inheritParams common_params
