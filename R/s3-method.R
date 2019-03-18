2cd1015ec7d7bef542487a4852f45b34fff4445e
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-18 23:51:14 +0900

    :wrench: Change augment columns; Add point and text to plot()

--- R/s3-method.R
+++ R/s3-method.R
@@ -27,16 +27,18 @@ augment.Rcpp_IGraph = function(x, ...) {
   root = x$source
   from = c(root, x$from)
   to = c(root, x$to)
-  df_from = x$Vattr[from, c("x", "y")]
-  df_to = stats::setNames(x$Vattr[to, c("x", "y")], c("xend", "yend"))
-  df = cbind(from = as_vnames(x, from), to = as_vnames(x, to), df_from, df_to)
+  df_from = stats::setNames(x$Vattr[from, c("x", "y")], c("xfrom", "yfrom"))
+  df_to = x$Vattr[to, c("x", "y")]
+  df = cbind(from = as_vnames(x, from), to = as_vnames(x, to), df_to, df_from)
   class(df) = c("tbl_df", "tbl", "data.frame")
   df
 }
 
 #' @export
-plot.Rcpp_IGraph = function(x, ...) {
+plot.Rcpp_IGraph = function(x, ..., lwd = 0.5, cex = 5, col = "#cccccc", pch = 16) {
   data = augment(x, ...)
-  ggplot2::ggplot(data) +
-    ggplot2::geom_segment(ggplot2::aes_(~x, ~y, xend = ~xend, yend = ~yend))
+  ggplot2::ggplot(data, ggplot2::aes_(~x, ~y)) +
+    ggplot2::geom_segment(ggplot2::aes_(xend = ~xfrom, yend = ~yfrom), size = lwd) +
+    ggplot2::geom_point(shape = pch, size = cex, colour = col) +
+    ggplot2::geom_text(ggplot2::aes_(label = ~to), size = cex * 0.6)
 }
