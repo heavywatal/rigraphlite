007099386d75f96b991c49ccafc561042d78a38f
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-04-18 13:03:18 +0900

    :boom: Change output column names from augment()

--- R/s3-method.R
+++ R/s3-method.R
@@ -37,9 +37,19 @@ augment.Rcpp_IGraph = function(x, layout = NULL, ...) {
   root = x$source
   from = c(root, x$from)
   to = c(root, x$to)
-  df_from = stats::setNames(layout[from, c("x", "y")], c("xfrom", "yfrom"))
-  df_to = layout[to, c("x", "y")]
-  df = cbind(from = as_vnames(x, from), to = as_vnames(x, to), df_to, df_from)
+  df = segment_df(from, to, layout[["x"]], layout[["y"]])
+  df$label = as_vnames(x, df$to)
+  df
+}
+
+segment_df = function(from, to, x, y) {
+  df = data.frame(
+    from, to,
+    x = x[to],
+    y = y[to],
+    xend = x[from],
+    yend = y[from]
+  )
   class(df) = c("tbl_df", "tbl", "data.frame")
   df
 }
@@ -48,7 +58,7 @@ augment.Rcpp_IGraph = function(x, layout = NULL, ...) {
 plot.Rcpp_IGraph = function(x, ..., lwd = 0.5, cex = 5, col = "#cccccc", pch = 16) {
   data = augment(x, ...)
   ggplot2::ggplot(data, ggplot2::aes_(~x, ~y)) +
-    ggplot2::geom_segment(ggplot2::aes_(xend = ~xfrom, yend = ~yfrom), size = lwd) +
+    ggplot2::geom_segment(ggplot2::aes_(xend = ~xend, yend = ~yend), size = lwd) +
     ggplot2::geom_point(shape = pch, size = cex, colour = col) +
-    ggplot2::geom_text(ggplot2::aes_(label = ~to), size = cex * 0.6)
+    ggplot2::geom_text(ggplot2::aes_(label = ~label), size = cex * 0.6)
 }
