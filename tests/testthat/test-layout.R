998d6cda56028bf741e6ecadd4ae84ed08af6d8f
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2021-12-03 15:01:56 +0900

    :white_check_mark: Test rng_seed()

--- tests/testthat/test-layout.R
+++ tests/testthat/test-layout.R
@@ -1,7 +1,12 @@
 test_that("layout functions return x-y columns", {
   g = graph_tree(7L)
+  expect_silent(rng_seed(19937L))
+  l1 = layout_random(g)
+  expect_silent(rng_seed(19937L))
+  l2 = layout_random(g)
+  expect_equal(l1, l2)
+  expect_named(l1, c("x", "y"))
   expect_named(layout_nicely(g), c("x", "y"))
-  expect_named(layout_random(g), c("x", "y"))
   expect_named(layout_drl(g), c("x", "y"))
   expect_named(layout_fruchterman_reingold(g), c("x", "y"))
   expect_named(layout_mds(g), c("x", "y"))
