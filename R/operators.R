ce298de2177de92d661a04e5f73947c7622d9980
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-04-26 23:57:16 +0900

    :memo: Add examples and returns to induced_subgraph()

--- R/operators.R
+++ R/operators.R
@@ -6,10 +6,18 @@
 #' @source <https://igraph.org/c/doc/igraph-Operators.html#miscellaneous-operators>
 #' @seealso [delete_vertices()]
 #' @inheritParams common_params
-#' @param impl how to construct a new graph:
+#' @param impl An integer value, how to construct a new graph:
 #'             \{0: AUTO, 1: COPY_AND_DELETE, 2: CREATE_FROM_SCRATCH\}
+#' @returns An [`igraph_ptr`] object.
 #' @rdname subgraph
 #' @export
+#' @examples
+#' g = graph_tree(5L)
+#' induced_subgraph(g, seq_len(3L))
+#'
+#' edges = induced_subgraph_edges(g, seq_len(3L)) |> print()
+#'
+#' subgraph_from_edges(g, edges)
 induced_subgraph = function(graph, vids, impl = 0L) {
   .Call(`_igraphlite_induced_subgraph_`, graph, sort(vids), impl) |> set_ptr_class()
 }
